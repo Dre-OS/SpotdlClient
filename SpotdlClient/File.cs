38b9bbb7ac531f4805950771586464826952bd8a
Dre-OS <hlsagabaen@gmail.com>
2023-06-12 08:29:51 +0800

    Added File class; moved DestinationAssign method

---
+++ SpotdlClient/File.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpotdlClient
+{
+    class File
+    {
+        public string Folder;
+        public File()
+        {
+            
+        }
+        public File(string folder)
+        {
+            this.Folder = folder;
+        }
+        public string Destination()
+        {
+            return this.Folder;
+        }
+        public void DestinationAssign()
+        {
+            //find a path to be used as download location
+            var fbd = new FolderBrowserDialog();
+            string folder = "";
+            DialogResult result = fbd.ShowDialog();
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                folder = fbd.SelectedPath;
+            }
+            this.Folder=folder;
+        }
+    }
+}
